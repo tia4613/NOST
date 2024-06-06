09fd4a39c8fea4f0c764507c74fe47c06a81d73d
hyemin lee <hy2min@gmail.com>
2024-06-06 22:08:04 +0900

    fix : 회원가입 이메일 인증 모달창 생성

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -31,3 +31,8 @@ urlpatterns = [
         name="swagger-ui",
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.conf import settings
+from django.conf.urls.static import static
