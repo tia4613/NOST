fa61a8df761bd03d30896008a85d9bb2e4d58e6e
hyemin lee <hy2min@gmail.com>
2024-06-07 10:57:20 +0900

    fix : config/urls import위치 수정, like,rating 유저제한 삭제

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -34,5 +37,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-from django.conf import settings
-from django.conf.urls.static import static
