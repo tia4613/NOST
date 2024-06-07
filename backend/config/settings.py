8ab4e73bc2a6ad1503784f045f067451a0a32e7a
Juunsik <Juunsik29@gmail.com>
2024-06-08 02:16:16 +0900

    Fix: secret import

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
-import secret
+from . import secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
